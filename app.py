41ec5826b7e6fb0ddde6a7607eb80a60a8811d03
hirajya <rodneyestrada2425@gmail.com>
2024-03-18 11:17:14 +0800

    goods final eps 3

--- app.py
+++ app.py
@@ -30,6 +30,11 @@ def read_csv(config: dict) -> pd.DataFrame:
     params = config["PARAMS"]
     return pd.read_csv(url, **params)
 
+def delete_all_files_in_directory(directory: Path):
+    for file in directory.iterdir():
+        if file.is_file():
+            file.unlink()
+
 def delete_file(file_source: str | Path):
     remove(file_source)
 
@@ -39,18 +44,26 @@ def pipeline():
         config = json.load(fp)
 
     ftp = get_ftp()
+    data_dir = Path("fdata")
+    data_dir.mkdir(parents=True, exist_ok=True)
+    delete_all_files_in_directory(data_dir)
 
-    # Loop through each config to read and save CSV
     for src_name, src_config in config.items():
+        # Create data directory if it doesn't exist
+        
+
         file_name = src_name + ".CSV"
-        read_csv(src_config).to_csv(file_name, index=False)
+        file_path = data_dir / file_name
+        
+        # Read CSV and save to file path
+        read_csv(src_config).to_csv(file_path, index=False)
 
         # Upload to FTP
-        upload_to_ftp(ftp, Path(file_name))
+        upload_to_ftp(ftp, file_path)
         print(f"Uploaded {file_name} to FTP.")
 
-        delete_file(file_name)
-        print(f"Deleted {file_name}.")
+        # delete_file(file_path)
+        # print(f"Deleted {file_name}.")
 
 if __name__ == "__main__":
 
